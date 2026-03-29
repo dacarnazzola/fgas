447eea9cae6b55e43d22522f1c4609ed1baa704a
Derick Carnazzola <maxediv@gmail.com>
2026-03-29 00:38:16 -0500

    fixed potential bug in random integer

--- src/modules/random.f90
+++ src/modules/random.f90
@@ -1,5 +1,5 @@
 module random
-use, non_intrinsic :: kinds, only: i32, dp
+use, non_intrinsic :: kinds, only: i32, i64, dp
 implicit none
 private
 public :: random_uniform_i32
@@ -9,11 +9,12 @@ contains
     impure subroutine random_uniform_i32(x, n, lo, hi)
         integer(kind=i32), intent(in) :: n, lo, hi
         integer(kind=i32), intent(inout) :: x(n)
-        real(kind=dp) :: work
+        real(kind=dp) :: work, hi_lo
         integer(kind=i32) :: i
+        hi_lo = real(int(hi, kind=i64) - int(lo, kind=i64) + 1_i64, kind=dp)
         do i=1_i32,n
             call random_number(work)
-            x(i) = int(work*(hi - lo + 1_i32), kind=i32) + lo
+            x(i) = int(int(work*hi_lo, kind=i64) + int(lo, kind=i64), kind=i32)
         end do
     end subroutine random_uniform_i32
 
