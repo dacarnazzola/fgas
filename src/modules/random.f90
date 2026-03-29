cd5efa5053ab5372d4b1cd72f556452a03077580
Derick Carnazzola <maxediv@gmail.com>
2026-03-29 11:18:07 -0500

    added tournament selection

--- src/modules/random.f90
+++ src/modules/random.f90
@@ -9,13 +9,10 @@ contains
     impure subroutine random_uniform_i32(x, n, lo, hi)
         integer(kind=i32), intent(in) :: n, lo, hi
         integer(kind=i32), intent(inout) :: x(n)
-        real(kind=dp) :: work, hi_lo
-        integer(kind=i32) :: i
+        real(kind=dp) :: work(n), hi_lo
+        call random_number(work)
         hi_lo = real(int(hi, kind=i64) - int(lo, kind=i64) + 1_i64, kind=dp)
-        do i=1_i32,n
-            call random_number(work)
-            x(i) = int(int(work*hi_lo, kind=i64) + int(lo, kind=i64), kind=i32)
-        end do
+        x = int(int(work*hi_lo, kind=i64) + int(lo, kind=i64), kind=i32)
     end subroutine random_uniform_i32
 
 end module random
---
+++ src/modules/selection.f90
@@ -0,0 +1,53 @@
+module selection
+use, non_intrinsic :: kinds, only: i32, i64, sp
+use, non_intrinsic :: system, only: debug_error_condition
+use, non_intrinsic :: random, only: random_uniform_i32
+implicit none
+private
+public :: selection_uniform_random, selection_tournament
+
+    integer(kind=i32), parameter :: DEFAULT_TOURNAMENT_SIZE = 2
+
+contains
+
+    impure subroutine selection_uniform_random(population, selected_pairs_ii)
+        real(kind=sp), intent(in) :: population(:,:)
+        integer(kind=i32), intent(inout) :: selected_pairs_ii(2,size(population, dim=2, kind=i64))
+        call debug_error_condition(size(population,dim=2,kind=i64) > huge(1_i32), &
+                                   'SELECTION::SELECTION_UNIFORM_RANDOM population is too large for i32 storage')
+        call random_uniform_i32(selected_pairs_ii, size(selected_pairs_ii, kind=i32), 1_i32, size(population, dim=2, kind=i32))
+    end subroutine selection_uniform_random
+
+    impure subroutine selection_tournament(population, fitness, selected_pairs_ii, k_opt)
+        real(kind=sp), intent(in) :: population(:,:), fitness(size(population, dim=2, kind=i64))
+        integer(kind=i32), intent(inout) :: selected_pairs_ii(2,size(population, dim=2, kind=i64))
+        integer(kind=i32), intent(in), optional :: k_opt
+        integer(kind=i32) :: k, i, t1_start, t1_end, t2_start, t2_end
+        integer(kind=i32), allocatable :: tournaments(:,:)
+        real(kind=sp), allocatable :: tournament_fitness(:)
+        call debug_error_condition(size(population,dim=2,kind=i64) > huge(1_i32), &
+                                   'SELECTION::SELECTION_TOURNAMENT population is too large for i32 storage')
+        call debug_error_condition(size(population,dim=2) /= size(fitness), &
+                                   'SELECTION::SELECTION_TOURNAMENT population size does not match fitness size')
+        if (present(k_opt)) then
+            call debug_error_condition(k_opt < 1_i32, &
+                                       'SELECTON::SELECTION_TOURNAMENT k must be >= 1')
+            k = k_opt
+        else
+            k = DEFAULT_TOURNAMENT_SIZE
+        end if
+        t1_start = 1_i32
+        t1_end = k
+        t2_start = k + 1_i32
+        t2_end = 2_i32*k
+        allocate(tournaments(2_i32*k,size(population,dim=2,kind=i32)))
+        call random_uniform_i32(tournaments, size(tournaments, kind=i32), 1_i32, size(population, dim=2, kind=i32))
+        allocate(tournament_fitness(2_i32*k))
+        do i=1_i32,size(population, dim=2, kind=i32)
+            tournament_fitness = fitness(tournaments(:,i))
+            selected_pairs_ii(1,i) = tournaments(minloc(tournament_fitness(t1_start:t1_end), dim=1),i)
+            selected_pairs_ii(2,i) = tournaments(minloc(tournament_fitness(t2_start:t2_end), dim=1)+k,i)
+        end do
+    end subroutine selection_tournament
+
+end module selection
