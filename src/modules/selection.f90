010df578137d6cace5a8ba49549d4f437e82a45c
Derick Carnazzola <maxediv@gmail.com>
2026-03-31 05:52:45 -0500

    fixed selection routines to accept assumed shape arrays

--- src/modules/selection.f90
+++ src/modules/selection.f90
@@ -12,15 +12,15 @@ contains
 
     impure subroutine selection_uniform_random(population, selected_pairs_ii)
         real(kind=sp), intent(in) :: population(:,:)
-        integer(kind=i32), intent(out) :: selected_pairs_ii(2,size(population, dim=2, kind=i64))
+        integer(kind=i32), intent(out) :: selected_pairs_ii(:,:)
         call debug_error_condition(size(population,dim=2,kind=i64) > huge(1_i32), &
                                    'SELECTION::SELECTION_UNIFORM_RANDOM population is too large for i32 storage')
         call random_uniform_i32(selected_pairs_ii, size(selected_pairs_ii, kind=i32), 1_i32, size(population, dim=2, kind=i32))
     end subroutine selection_uniform_random
 
     impure subroutine selection_tournament(population, fitness, selected_pairs_ii, k_opt)
-        real(kind=sp), intent(in) :: population(:,:), fitness(size(population, dim=2, kind=i64))
-        integer(kind=i32), intent(out) :: selected_pairs_ii(2,size(population, dim=2, kind=i64))
+        real(kind=sp), intent(in) :: population(:,:), fitness(:)
+        integer(kind=i32), intent(out) :: selected_pairs_ii(:,:)
         integer(kind=i32), intent(in), optional :: k_opt
         integer(kind=i32) :: k, i, t1_start, t1_end, t2_start, t2_end
         integer(kind=i32), allocatable :: tournaments(:,:)
@@ -39,9 +39,9 @@ contains
         t1_end = k
         t2_start = k + 1_i32
         t2_end = 2_i32*k
-        allocate(tournaments(2_i32*k,size(population,dim=2)))
+        allocate(tournaments(2_i32*k,size(selected_ii,dim=2)))
         call random_uniform_i32(tournaments, size(tournaments, kind=i32), 1_i32, size(population, dim=2, kind=i32))
-        do concurrent (i=1_i32:size(population, dim=2))
+        do concurrent (i=1_i32:size(selected_ii, dim=2))
             selected_pairs_ii(1,i) = tournaments(minloc(fitness(tournaments(t1_start:t1_end,i)), dim=1),i)
             selected_pairs_ii(2,i) = tournaments(minloc(fitness(tournaments(t2_start:t2_end,i)), dim=1)+k,i)
         end do
